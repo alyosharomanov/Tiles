772b354298db5cc149a1a14b71bf881d7199afc7
alyosharomanov <tato.romanov@gmail.com>
2016-05-23 22:21:40 -0700

    Update 1.1

--- app/src/main/java/game/alyosharomanov/tiles/SocialActivity.java
+++ app/src/main/java/game/alyosharomanov/tiles/SocialActivity.java
@@ -4,6 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Activity responsible for social aspects like ratings
@@ -20,6 +26,9 @@ public class SocialActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social);
 
+        Button rateButton = (Button) findViewById(R.id.rateButton);//button name in xml file
+        rateButton.setOnClickListener(rate); // on button click listener
+
         // Set up the back button
         Button backButton = (Button) findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -31,4 +40,15 @@ public class SocialActivity extends AppCompatActivity {
 
         return;
     }
+
+    private Button.OnClickListener rate = new Button.OnClickListener() {
+        public void onClick(View v) {
+
+            Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=game.alyosharomanov.tiles");
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+
+
+        }
+    };
 }
